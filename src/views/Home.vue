b98d15e965f22f1f57f0547adc91f84e723908bf
Ian Boyle <irboyle21@gmail.com>
2021-12-25 15:47:34 -0700

    adds csv parsing functionality to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>
+        File
+        <input type="file" accept=".csv" @change="handleFileUpload($event)" />
+      </label>
+      <button v-on:click="submitFile()">Submit</button>
+    </div>
+    <p v-for="(row, rowKey) in data" v-bind:key="'row-' + rowKey">{{ row }}</p>
+    <table v-if="parsed" style="width: 100%">
+      <thead>
+        <tr>
+          <th v-for="(header, key) in content.meta.fields" v-bind:key="'header-' + key">
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowKey) in data" v-bind:key="'row-' + rowKey">
+          <td v-for="(column, columnKey) in content.meta.fields" v-bind:key="'row-' + rowKey + '-column-' + columnKey">
+            <!-- column is the column name -->
+            <p>{{ row["Symbol"] }}, {{ column }}</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <table v-if="parsed" style="width: 100%">
+      <thead>
+        <tr>
+          <th v-for="(header, key) in content.meta.fields" v-bind:key="'header-' + key">
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowKey) in content.data" v-bind:key="'row-' + rowKey">
+          <td v-for="(column, columnKey) in content.meta.fields" v-bind:key="'row-' + rowKey + '-column-' + columnKey">
+            <input v-model="content.data[rowKey][column]" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import axios from "axios";
+import Papa from "papaparse";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      file: "",
+      content: [],
+      parsed: false,
+      data: [],
+    };
+  },
+  methods: {
+    handleFileUpload(event) {
+      this.file = event.target.files[0];
+      this.parseFile();
+    },
+    submitFile() {
+      let formData = new FormData();
+      formData.append("file", this.file);
+      console.log(formData);
+      // axios
+      //   .post("/single-file", formData, {
+      //     headers: {
+      //       "Content-Type": "multipart/form-data",
+      //     },
+      //   })
+      //   .then(function () {
+      //     console.log("SUCCESS!!");
+      //   })
+      //   .catch(function () {
+      //     console.log("FAILURE!!");
+      //   });
+    },
+    parseFile() {
+      Papa.parse(this.file, {
+        header: true,
+        skipEmptyLines: true,
+        complete: function (results) {
+          this.content = results;
+          this.data = this.content.data;
+          console.log(this.content.data);
+          this.parsed = true;
+        }.bind(this),
+      });
+    },
   },
 };
 </script>
